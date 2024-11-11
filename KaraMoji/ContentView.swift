42e2631e54254019a5635b11799c9d6eee9736dc
仲優樹 <yu-ki216m@outlook.jp>
2024-11-11 11:03:58 +0900

    Initial Commit

---
+++ KaraMoji/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KaraMoji
+//
+//  Created by 仲優樹 on 2024/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KaraMoji/KaraMojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaraMojiApp.swift
+//  KaraMoji
+//
+//  Created by 仲優樹 on 2024/11/11.
+//
+
+import SwiftUI
+
+@main
+struct KaraMojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
